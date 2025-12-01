c931f6a019f27adbd82d9cbb07cfff2991ae5191
Ebrahim <ebuebrahim786@gmail.com>
2025-12-01 13:42:05 +0800

    new changes

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
-import EntryField from "@/components/EntryField";
 import MetalEntryField from "@/components/MetalEntryField";
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center bg-zinc-50 dark:bg-black ">
-      <p className="text-xl m-5 font-bold">Metal Calculator</p>
+    <div className="flex flex-col min-h-screen items-center bg-zinc-50 dark:bg-black gap-5">
+      <div className=" bg-blue-950 text-white w-full">
+        <p className="text-4xl m-5 font-bold  text-white text-center">
+          Metal Calculator
+        </p>
+      </div>
 
       <MetalEntryField />
     </div>
